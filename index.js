8ab1c0417ac83d1c3e4e2234a6f28d785157d775
almahmudzihad <amzihad@gmail.com>
2026-05-09 23:30:22 +0600

    sent data to database

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 const port = process.env.PORT || 5000;
-
+app.use(cors());
+app.use(express.json());
 const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,14 +19,21 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const database = client.db("wanderlust");
+    const collection = database.collection("destinations");
+
+    app.post('/destinations', async (req, res) => {
+        const newDestination = req.body;
+        const result = await collection.insertOne(newDestination);
+        res.json(result);
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
